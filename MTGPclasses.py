91fe2c9312b5d75644cc167a6f9153ebcbee2682
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2021-12-13 13:29:40 -0700

    Add module that includes Multi-task GPs

---
+++ MTGPclasses.py
@@ -0,0 +1,15 @@
+class BatchIndependentMultitaskGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super().__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([2]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([2])),
+            batch_shape=torch.Size([2])
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultitaskMultivariateNormal.from_batch_mvn(
+            gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        )

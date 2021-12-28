799598cab6db372b8bd669c7f87605e91f3edab5
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2021-12-27 21:06:28 -0700

    Revert "Create GPindK"
    This reverts commit 8160c31bc7066f79b11bbb0dca755ddd93973ec8.

--- MTGPclasses.py
+++ MTGPclasses.py
@@ -1,20 +1,11 @@
-import torch
-import gpytorch
 class BatchIndependentMultitaskGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood,n_tasks,kernel_type):
+    def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([n_tasks]))
-        
-        if kernel_type == 'RBF':         
-            self.covar_module = gpytorch.kernels.ScaleKernel(
-                gpytorch.kernels.RBFKernel(batch_shape=torch.Size([n_tasks])),
-                batch_shape=torch.Size([n_tasks])
-            )
-        if kernel_type == 'lin':
-            self.covar_module = gpytorch.kernels.ScaleKernel(
-                gpytorch.kernels.LinearKernel()(batch_shape=torch.Size([n_tasks])),
-                batch_shape=torch.Size([n_tasks])
-            )
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([2]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([2])),
+            batch_shape=torch.Size([2])
+        )
 
     def forward(self, x):
         mean_x = self.mean_module(x)

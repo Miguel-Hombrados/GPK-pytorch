54dbaebc58579f44dd52fc1d3f45812b29f4d6ad
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-01-04 12:16:50 -0700

    Create to_torch.py
    Convert numpy arrays to torch tensors.

---
+++ utils/to_torch.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 18:32:41 2022
+
+@author: mahom
+"""
+
+import torch
+import numpy as np
+def to_torch(x):
+    
+    if isinstance(x, (np.ndarray, np.generic) ):
+        y= torch.from_numpy(x).float()
+    else:
+        y=x.double()
+    return y

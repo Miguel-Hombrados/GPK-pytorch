2b9d311da8aad6eaf273af0f5e2fd095d2722fac
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-01-04 18:00:25 -0700

    Update MAPE.py

--- utils/MAPE.py
+++ utils/MAPE.py
@@ -5,9 +5,9 @@ Created on Wed Dec 27 00:38:58 2017
 @author: MiguelAngel
 """
 
-import numpy as np
+import torch
 def MAPE(Yreal,Yestimated):
     #☺both are matrices NXS N stands for number of time instants and S by number of time series
-    error=(100/np.size(Yestimated,0))*np.sum(np.abs(np.divide(Yestimated-Yreal,Yreal)),0)
+    error=(100/torch.Tensor.size(Yestimated,0))*torch.sum(torch.abs(torch.divide(Yestimated-Yreal,Yreal)),0)
     
     return error

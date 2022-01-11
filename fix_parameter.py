4747f093ec227eabc09ccc9fcc75e4474f916aae
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-01-10 19:05:43 -0700

    Create fix_parameter.py
    Function that fix parameters that we do not want to optimize.

---
+++ fix_parameter.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  8 09:20:44 2022
+
+@author: mahom
+"""
+import torch
+def fix_parameter(model,kernel_type):
+    
+    shared_noise_variance = 0.1
+    task_num = model.likelihood.num_tasks
+    if kernel_type == "linear":
+        model.covar_module.kernels[0].base_kernel.variance = torch.ones(task_num,1,1) # If selected "outputscale" the tensor should be 1d tensor of length task_num 
+        model.likelihood.noise = shared_noise_variance
+        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}-{model.covar_module.kernels[0].base_kernel.raw_variance}
+        
+    if kernel_type == "rbf":
+        model.likelihood.noise = shared_noise_variance
+        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}
+    
+    if kernel_type == "matern":
+        model.likelihood.noise = shared_noise_variance
+        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}
+        #new_parameters = set(list(model.parameters()))
+    return model,new_parameters
+        
+        

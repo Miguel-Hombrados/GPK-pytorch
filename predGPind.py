6430044629ebf0f768f7372ffcc90ca8bbd684b1
Miguel Hombrados <mangelhombradosherre@unm.edu>
2022-01-21 19:44:07 -0700

    Update predGPind.py
    Reshape the covariance matrix into a square shape.

--- predGPind.py
+++ predGPind.py
@@ -10,6 +10,7 @@ from to_torch import to_torch
 def predGPind(test_x,likelihood,model):
     
     test_x = to_torch(test_x)
+    Ntest = torch.Tensor.size(test_x,0)
     # Make predictions
     model.eval()
     likelihood.eval()
@@ -18,5 +19,5 @@ def predGPind(test_x,likelihood,model):
         predictions = likelihood(model(test_x))
         mean = predictions.mean
         cov = predictions.covariance_matrix
-        var = cov.diag().reshape(-1,1)
+        var = cov.diag().reshape(Ntest,-1)
     return mean,var

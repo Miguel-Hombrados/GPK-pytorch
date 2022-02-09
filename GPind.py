8310bfd0e5ca983b2a31a9acdd8cc513cce56fe9
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-02-08 21:57:55 -0700

    Add valid and train epoch functions

--- GPind.py
+++ GPind.py
@@ -5,54 +5,123 @@ Created on Mon Dec 20 19:05:02 2021
 @author: mahom
 """
 
-
+import numpy as np
 import torch
 import gpytorch
 from to_torch import to_torch
 from fix_constraints import fix_constraints
 from MTGPclasses import BatchIndependentMultitaskGPModel
-
+from sklearn.model_selection import KFold
+from torch.utils.data import Dataset, DataLoader,TensorDataset,random_split,SubsetRandomSampler, ConcatDataset
 from fix_parameter import fix_parameter
 from my_initialization import my_initialization
-
+from epoch_tv import train_epoch,valid_epoch
 def GPind(train_x,train_y,n_tasks,kernel_type):
-    
-    train_x = to_torch(train_x)
-    train_y = to_torch(train_y)
-    
+    validation = True
+    #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    training_iterations = 21
+    
+    if validation == True:
+        k=5
+        splits=KFold(n_splits=k,shuffle=True,random_state=42)
+    
+    
+    train_x= to_torch(train_x)
+    train_y = to_torch(train_y)
+    #dataset = TensorDataset(train_x, train_y)
+    #dataset = DataLoader((train_x,train_y))
+
+    num_iter = 21
     learning_rate = 0.1
 
-    
-    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=n_tasks)
-    model = BatchIndependentMultitaskGPModel(train_x, train_y, likelihood,n_tasks,kernel_type)
-    
-    fix_constraints(model,likelihood,kernel_type,n_tasks)
-    hypers = my_initialization(model,kernel_type,n_tasks)
-    #model.initialize(**hypers)
+    foldperf = {}
+    best_models = {}
+    best_niter = {}
+    for fold, (train_idx,val_idx) in enumerate(splits.split(train_x)):
+
+        print('Fold {}'.format(fold + 1))
+        #device = torch.device("cpu")
+        
+        
+        train_x_k = train_x[train_idx,:]
+        train_y_k = train_y[train_idx,:]
+        data_train = (train_x_k,train_y_k)
+        valid_x_k = train_x[val_idx,:]
+        valid_y_k = train_y[val_idx,:]
+        data_val = (valid_x_k,valid_y_k)
+        likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=n_tasks)
+        model = BatchIndependentMultitaskGPModel(train_x_k,train_y_k, likelihood,n_tasks,kernel_type)
+        
+        fix_constraints(model,likelihood,kernel_type,n_tasks)
+        hypers = my_initialization(model,kernel_type,n_tasks)
+        #model.initialize(**hypers)
+         
+        
+        # Find optimal model hyperparameters
+        model.train()
+        likelihood.train()
+        
+        
+        # Fix redundant parameters
+        [model,new_parameters] = fix_parameter(model,kernel_type)
+        # Use the adam optimizer
+        optimizer = torch.optim.Adam(new_parameters, lr=learning_rate)  # Includes GaussianLikelihood parameters
+        
+        history = {'train_loss': [], 'valid_loss': []}
+
+        
+        # "Loss" for GPs - the marginal log likelihood
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+        
+        for it in range(num_iter):
+            train_loss,output = train_epoch(model,data_train,mll,optimizer)
+            optimizer.zero_grad()
+            valid_loss = valid_epoch(model,likelihood,output,data_val,mll)
+        
+            train_loss = train_loss / data_train[0].size()[0]
+            valid_loss = valid_loss / data_val[0].size()[0]
+            
+            print("Iter:{}/{} AVG Training Loss:{:.3f} AVG Valid Loss:{:.3f}".format(it + 1,
+                                                                             num_iter,
+                                                                             train_loss,
+                                                                             valid_loss,
+                                                                              ))
+            
+            if it> 1  and valid_loss < np.min(history['valid_loss']):
+                min_valid_loss = valid_loss
+                opt_niter = it
+                best_params = model.state_dict()
+            
+            history['train_loss'].append(train_loss)
+            history['valid_loss'].append(valid_loss)
+        foldperf['fold{}'.format(fold+1)] = history  
+                                
+        # for i in range(training_iterations):
+        #     optimizer.zero_grad()
+        #     output = model(train_x)
+        #     loss = -mll(output, train_y)
+        #     loss.backward()
+        #     print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iterations, loss.item()))
+        #     optimizer.step()
+        
+        best_models['fold{}'.format(fold+1)] = best_params
+        best_niter['fold{}'.format(fold+1)] = opt_niter
+    n_opt = np.median(list(best_niter.values()))
+    for it in range(n_opt):
+        likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=n_tasks)
+        model = BatchIndependentMultitaskGPModel(train_x, train_y, likelihood,n_tasks,kernel_type) 
+        fix_constraints(model,likelihood,kernel_type,n_tasks)
+        hypers = my_initialization(model,kernel_type,n_tasks)
+        # Find optimal model hyperparameters
+        model.train()
+        likelihood.train()
+        
+        
+        # Fix redundant parameters
+        [model,new_parameters] = fix_parameter(model,kernel_type)
+        # Use the adam optimizer
+        optimizer = torch.optim.Adam(new_parameters, lr=learning_rate)  # Includes GaussianLikelihood parameters
+        
+    return model,likelihood#, history, foldperf, best_models
 
 
-    
-    # Find optimal model hyperparameters
-    model.train()
-    likelihood.train()
-    
-    
-    # Fix redundant parameters
-    [model,new_parameters] = fix_parameter(model,kernel_type)
-    # Use the adam optimizer
-    optimizer = torch.optim.Adam(new_parameters, lr=learning_rate)  # Includes GaussianLikelihood parameters
-
-    # "Loss" for GPs - the marginal log likelihood
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-
-    for i in range(training_iterations):
-        optimizer.zero_grad()
-        output = model(train_x)
-        loss = -mll(output, train_y)
-        loss.backward()
-        print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iterations, loss.item()))
-        optimizer.step()
-    
-    return model, likelihood
--- epoch_tv.py
+++ epoch_tv.py
@@ -4,30 +4,28 @@ Created on Sun Feb  6 16:49:14 2022
 
 @author: mahom
 """
-
-def train_epoch(model,device,dataloader,mll,optimizer):
+import torch
+import gpytorch
+def train_epoch(model,data,mll,optimizer):
     train_loss=0.0
     model.train()
-    for images, labels in dataloader:
+#    for images, labels in dataloader:
 
-        train_x,train_y = images.to(device),labels.to(device)
-        optimizer.zero_grad()
-        output = model(train_x)
-        loss = -mll(output, train_y)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item() * train_x.size(0)
+    #train_x,train_y = images.to(device),labels.to(device)
+    (train_x,train_y) = data 
+    optimizer.zero_grad()
+    output = model(train_x)
+    loss = -mll(output, train_y)
+    loss.backward()
+    optimizer.step()
+    train_loss += loss.item() * train_x.size(0)
 
-    return train_loss
+    return train_loss,output
 
-def valid_epoch(model,device,dataloader,mll):
+def valid_epoch(model,likelihood,output,data,mll):
    valid_loss = 0.0
-   model.eval()
-   for images, labels in dataloader:
-
-       val_x,val_y  = images.to(device),labels.to(device)
-       output = model(val_x)
-       loss = -mll(output, val_y)
-       valid_loss+=loss.item()*val_x.size(0)
+   (val_x,val_y) = data 
+   likelihood(val_y)
+   #valid_loss+=loss.item()*val_x.size(0)
 
    return valid_loss

4596725a3244b691a6c7d34551cd812b11a37527
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-02-19 12:12:19 -0700

    Update correcting_factor_cov.py
    Add options for different version of GPK

--- correcting_factor_cov.py
+++ correcting_factor_cov.py
@@ -5,17 +5,26 @@ Created on Thu Feb 17 20:18:44 2022
 @author: mahom
 """
 import torch
-def correcting_factor_cov(model,W,Yval,Xval):
+def correcting_factor_cov(model,W,Yval,Xval,option_lv):
     # W: T x K
-    # Yval: T x Nval
-    # Xval  F x Nval
-    # K : Nval x T  times  N x T
+    # Yval: Nval x T
+    # Xval  Nval x F
+    # Kh : Nval x T  times  N x T
     Nval = Yval.size(1)
     n_task = Yval.size(0)
-    SigmaH = model.likelihood(model.forward(Xval)).covariance_matrix
-    
     yval = Yval.reshpe(-1,1)
     Wp = torch.kron(torch.eye(Nval),W)
+    if option_lv == "ind" or "mt":
+        SigmaH = model.likelihood(model.forward(Xval)).covariance_matrix
+    if option_lv == "ind_ori":
+        SigmaH_list = []
+        for task in range(0,n_task):
+            mk = model[task]
+            SigmaH_k = mk.likelihood(mk.forward(Xval)).covariance_matrix
+            SigmaH_list.append(SigmaH_k)
+        SigmaH = torch.block_diag(SigmaH_list)
+
+    
     Kyi = torch.inverse(Wp@SigmaH@Wp.T+1e-6*torch.eye(n_task*Nval))
     a = (yval.T@Kyi@yval)/(Nval*n_task)
     

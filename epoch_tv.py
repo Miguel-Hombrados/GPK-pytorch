84ae6d1f26e888410ae1f8e3982e699abc18ac1f
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-02-19 00:20:09 -0700

    Update epoch_tv.py
    Corrected so the loss of the validation samples are estimated with the marginal likelihoood.
    Added error estimation of the validation samples

--- epoch_tv.py
+++ epoch_tv.py
@@ -4,7 +4,7 @@ Created on Sun Feb  6 16:49:14 2022
 
 @author: mahom
 """
-
+import torch
 def train_epoch(model,data,mll,optimizer):
     train_loss=0.0
     model.train()
@@ -22,10 +22,15 @@ def train_epoch(model,data,mll,optimizer):
     return train_loss,output
 
 def valid_epoch(model,likelihood,output,data,mll):
+    
    (val_x,val_y) = data 
    f_val_est = model.forward(val_x)
-   y_val_est = likelihood(f_val_est)
-   loss = -mll(y_val_est, val_y)
+   #y_val_est = likelihood(f_val_est)
+   loss = -mll(f_val_est,val_y)
    valid_loss=loss.item()*val_x.size(0)
+   
+   
+   y_val_est  = likelihood(f_val_est)
+   valid_error = val_y-y_val_est.mean
 
-   return valid_loss
+   return valid_loss,valid_error

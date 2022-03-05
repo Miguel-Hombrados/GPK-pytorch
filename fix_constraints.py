fbba3e5ec6acc3f67adae62186581e2cbac9a7c0
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:34:37 -0700

    Update fix_constraints.py
    Add option for gpi_ori

--- fix_constraints.py
+++ fix_constraints.py
@@ -8,22 +8,31 @@ Created on Mon Jan 10 14:55:12 2022
 
 import gpytorch
 
-def fix_constraints(model,likelihood,kernel_type,num_task):
+def fix_constraints(model,likelihood,kernel_type,num_task,method):
     
-    if kernel_type == "linear":
-        likelihood.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-3,2))
-        likelihood.register_constraint("raw_task_noises", gpytorch.constraints.Interval(1e-3,2))
-       
-        model.covar_module.kernels[0].base_kernel.register_constraint("raw_variance", gpytorch.constraints.Interval(1e-3,2))
-        model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(1e-3,2))
-        model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-3,2))
-    if kernel_type == "rbf":
-        likelihood.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-7,5e-1))                                         #(1e-4,1)
-        likelihood.register_constraint("raw_task_noises", gpytorch.constraints.Interval(1e-3,1))                                   #(1e-1,2)
-       
-        model.covar_module.kernels[0].base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(10,100))    #(1.5,50)
-        model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(10,50))                #(1e-3,1)
-        model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-9,1e-3))                       #(1e-6,1)
-## Changing the constraint after the module has been created
-       
-#print(f'Noise constraint: {likelihood.noise_covar.raw_noise_constraint}')
+    
+    if method == "gpi":
+        if kernel_type == "linear":
+            likelihood.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-6,2))
+            likelihood.register_constraint("raw_task_noises", gpytorch.constraints.Interval(1e-2,2))
+           
+            model.covar_module.kernels[0].base_kernel.register_constraint("raw_variance", gpytorch.constraints.Interval(1e-3,50))
+            model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(1e-3,50))
+            model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-7,1))
+        if kernel_type == "rbf":
+            likelihood.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-8,5e-1))                                         #(1e-4,1)
+            likelihood.register_constraint("raw_task_noises", gpytorch.constraints.Interval(1e-8, 1))                                   #(1e-1,2)
+            model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(0.01,100))   
+            #model.covar_module.kernels[0].base_kernel.register_constraint("raw_outputscale", gpytorch.constraints.Interval(1,100)) 
+            model.covar_module.kernels[0].base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(1,100))    #(1.5,50)               #(1e-3,1)
+            model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-8, 1))                       #(1e-6,1)
+    if method == "gpi_ori":
+        if kernel_type == "rbf":  
+                model.likelihood.noise_covar.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-4, 10))                                                            # 0.5    
+                model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(1e-5, 50))
+                model.covar_module.kernels[0].base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(1,100))                  #3
+                model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-7, 1))   
+                
+
+               
+                

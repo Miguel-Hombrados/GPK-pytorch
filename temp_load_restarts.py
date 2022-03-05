4c9d37cb1f758eea9238ee50bf429b07e05c8071
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:32:02 -0700

    Create temp_load_restarts.py

---
+++ temp_load_restarts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 13:01:52 2022
+
+@author: mahom
+"""
+
+
+
+restart = 5
+
+model_train_24ind = M['restart{}'.format(restart)]
+like_train_24ind = L['restart{}'.format(restart)]

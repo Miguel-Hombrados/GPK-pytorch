7b5ead4a6df517c01e7a90bf53c40cc1144c68b9
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:32:16 -0700

    Create temp_load_params_gpi.py
    Print parameters of trained models

---
+++ temp_load_params_gpi.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 20 15:14:25 2022
+
+@author: mahom
+"""
+
+
+print("lenthscale",model.covar_module.kernels[0].base_kernel.lengthscale[0:3])
+print("outputscale",model.covar_module.kernels[0].outputscale[0:3])
+print("bias",model.covar_module.kernels[1].bias[0:3])
+print("noise",model.likelihood.task_noises[0:3])
+
+print("outputscale_fix",model.covar_module.kernels[0].base_kernel.outputscale)
+
+
+print("lenthscale",model.covar_module.kernels[0].base_kernel.lengthscale)
+print("outputscale",model.covar_module.kernels[0].outputscale)
+print("bias",model.covar_module.kernels[1].bias)
+print("noise",model.likelihood.task_noises)
+
+
+model.likelihood.raw_task_noises.grad
+model.covar_module.kernels[0].base_kernel.raw_outputscale.grad
+
+
+
+
+for task in range(1,25):
+
+    #print("lenthscale",model['task{}'.format(task)].covar_module.kernels[0].base_kernel.lengthscale)
+    #print("outputscale",model['task{}'.format(task)].covar_module.kernels[0].outputscale)
+    #print("bias",model['task{}'.format(task)].covar_module.kernels[1].bias)
+    print("noise",model['task{}'.format(task)].likelihood.noise)

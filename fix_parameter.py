d7b00843d952b41ea0c7995ac8c28c854a8e73f2
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:34:23 -0700

    Update fix_parameter.py
    Add option for gpi_ori

--- fix_parameter.py
+++ fix_parameter.py
@@ -5,23 +5,30 @@ Created on Sat Jan  8 09:20:44 2022
 @author: mahom
 """
 import torch
-def fix_parameter(model,kernel_type):
+def fix_parameter(model,kernel_type,method):
     
-    shared_noise_variance = 1e-6  # 1e-4
-    task_num = model.likelihood.num_tasks
-    if kernel_type == "linear":
-        model.covar_module.kernels[0].base_kernel.variance = torch.ones(task_num,1,1) # If selected "outputscale" the tensor should be 1d tensor of length task_num 
-        model.likelihood.noise = shared_noise_variance
-        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}-{model.covar_module.kernels[0].base_kernel.raw_variance}
+    
+    
+    if method == "gpi": 
+        shared_noise_variance = 0  # 1e-4
+        task_num = model.likelihood.num_tasks
+        if kernel_type == "linear":
+            model.covar_module.kernels[0].base_kernel.variance = torch.ones(task_num,1,1) # If selected "outputscale" the tensor should be 1d tensor of length task_num 
+            model.likelihood.noise = shared_noise_variance
+            new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}-{model.covar_module.kernels[0].base_kernel.raw_variance}
+            
+        if kernel_type == "rbf":
+            model.likelihood.noise = shared_noise_variance
+            new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}-{model.mean_module.constant}
         
-    if kernel_type == "rbf":
-        model.likelihood.noise = shared_noise_variance
-        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}
-    
-    if kernel_type == "matern":
-        model.likelihood.noise = shared_noise_variance
-        new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}
-        #new_parameters = set(list(model.parameters()))
+        
+        if kernel_type == "matern":
+            model.likelihood.noise = shared_noise_variance
+            new_parameters = set(list(model.parameters()))-{model.likelihood.raw_noise}
+            #new_parameters = set(list(model.parameters()))
+    if method == "gpi_ori": 
+            if kernel_type == "rbf":
+                new_parameters = set(list(model.parameters()))-{model.mean_module.constant}
     return model,new_parameters
-        
+           
         

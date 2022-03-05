80799600d44dcf33b3f1f0e10f89c0f95830c292
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:32:19 -0700

    Update predGPind.py

--- predGPind.py
+++ predGPind.py
@@ -7,22 +7,22 @@ Created on Tue Dec 28 18:50:45 2021
 import torch
 import gpytorch
 from to_torch import to_torch
+from opt_example import opt_example
 def predGPind(test_x,likelihood,model):
     
     test_x = to_torch(test_x)
     Ntest = torch.Tensor.size(test_x,0)
     # Make predictions
+    opt_example(model)
     model.eval()
     likelihood.eval()
     task_num = 24
-    model.likelihood.task_noises = 0.5*torch.ones(task_num)                                                         # 0.5
-    model.covar_module.kernels[0].base_kernel.outputscale = 5e-1*torch.ones(task_num)                                #0.5
-    model.covar_module.kernels[0].base_kernel.lengthscale = 15*torch.ones(task_num,1,1) # probar 15                  #3
-    model.covar_module.kernels[1].bias = 1e-3*torch.ones(task_num,1,1)                                              #1e-3
-
+    
+    
     with torch.no_grad(), gpytorch.settings.fast_pred_var():
         predictions = likelihood(model(test_x))
         mean = predictions.mean
         cov = predictions.covariance_matrix
         var = cov.diag().reshape(Ntest,-1)
+
     return mean,var

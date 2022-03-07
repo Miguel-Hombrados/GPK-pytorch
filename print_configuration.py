171fec10f3738d457c6f9024ca1d95cc28339192
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-07 15:00:11 -0700

    Create print_configuration.py
    Funciton that prints the optimization parameters.

---
+++ print_configuration.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 10:56:05 2022
+
+@author: mahom
+"""
+def print_configuration(forecast_method,kernel_type,EXPERIMENT,Stand,folder_data_name):
+    forecast_method
+    print("Forecasting method:", forecast_method)
+    print("Kernel type:", kernel_type)
+    print("Is data Standarized:", Stand)
+    print("Data folder:", folder_data_name)
+    print("Experiment:",   EXPERIMENT)
+    

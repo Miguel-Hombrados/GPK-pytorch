0c6111466a33eb44378375be073def64da4bf83a
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-09 11:25:17 -0700

    Update GPKtorch.py

--- GPKtorch.py
+++ GPKtorch.py
@@ -7,32 +7,30 @@ Created on Wed Dec 29 10:05:03 2021
 """
 
 import torch
-import gpytorch
 from GPind_ori import GPind_ori
 from GPind import GPind
 from GPMT import GPMT
 from to_torch import to_torch
 from sklearn.model_selection import train_test_split
-from correcting_factor_cov import correcting_factor_cov
-def GPKtorch(x,y,W,n_tasks,kernel_type,option_lv):
+def GPKtorch(x,y,W,n_tasks,kernel_type,option_lv,opt_parameters):
     
-    valtsize = 0.1
-    trainsize = 0.9
-    train_x = to_torch(x)
-    train_y = to_torch(y)
+    trainsize = opt_parameters['trainsize']  
+    valsize = 1 - trainsize
+    x = to_torch(x)
+    y = to_torch(y)
     
-    [train_x,val_x, train_y,val_y] = train_test_split(x,y, test_size=valtsize, train_size=trainsize, random_state=47, shuffle=True, stratify=None)
-      
+     
+    x = torch.cat((x,torch.Tensor(range(0,x.size(0))).reshape(-1,1)),dim=1)
+    [train_x,val_x, train_y,val_y] = train_test_split(x,y, test_size=valsize, train_size=trainsize, random_state=47, shuffle=True, stratify=None)
+    train_x = train_x[:,0:-1]
+    val_x  = val_x[:,0:-1]
+    ind_val = val_x[:,-1]
     
-    if option_lv == "ind":
-        [MODELS,LIKELIHOODS,Results,Opt_model,Opt_likelihood] = GPind_ori(train_x,train_y,n_tasks,kernel_type)
-        MODELS,LIKELIHOODS,Results,Opt_model,Opt_likelihood
-    if option_lv == "ind_ori":
-        [model,likelihood,n_opt_niter,min_valid_loss] = GPind(train_x,train_y,n_tasks,kernel_type)
-        
-    if option_lv == "mt":
-        [model,likelihood,n_opt_niter,min_valid_loss] = GPMT(train_x,train_y,n_tasks,kernel_type,option_lv)
+    if option_lv == "gp_ind_ori":
+        [MODELS,LIKELIHOODS,Results,Opt_model,Opt_likelihood] = GPind_ori(train_x,train_y,n_tasks,kernel_type,opt_parameters)
+    if option_lv == "gp_ind":
+        [model,likelihood,n_opt_niter,min_valid_loss] = GPind(train_x,train_y,n_tasks,kernel_type,opt_parameters)       
+    if option_lv == "gpmt":
+        [model,likelihood,n_opt_niter,min_valid_loss] = GPMT(train_x,train_y,n_tasks,kernel_type,option_lv,opt_parameters)
 
-
-    a = correcting_factor_cov(model,W,val_y,val_x)
-    return Opt_model,Opt_likelihood
+    return MODELS,LIKELIHOODS,Results,Opt_model,Opt_likelihood,ind_val

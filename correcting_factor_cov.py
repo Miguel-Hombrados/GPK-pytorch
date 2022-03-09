8f6cf7438493a9e7b43f3de01608739820ced9de
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-09 11:24:41 -0700

    Update correcting_factor_cov.py
    Funcionando.

--- correcting_factor_cov.py
+++ correcting_factor_cov.py
@@ -4,29 +4,32 @@ Created on Thu Feb 17 20:18:44 2022
 
 @author: mahom
 """
+import numpy as np
 import torch
-def correcting_factor_cov(model,W,Yval,Xval,option_lv):
+from to_torch import to_torch
+from DeStandarizeData import DeStandarizeData
+def correcting_factor_cov(model,W,Yval,Xval,option_lv,scalerY_K):
     # W: T x K
     # Yval: Nval x T
     # Xval  Nval x F
     # Kh : Nval x T  times  N x T
-    Nval = Yval.size(1)
-    n_task = Yval.size(0)
-    yval = Yval.reshpe(-1,1)
-    Wp = torch.kron(torch.eye(Nval),W)
-    if option_lv == "ind" or "mt":
+    
+    Nval = Yval.size(0)
+    n_task = Yval.size(1)
+    yval = Yval.reshape(-1,1)
+    Wp = to_torch(np.kron(torch.eye(Nval),W))
+    if option_lv == "ind" or option_lv == "mt":
         SigmaH = model.likelihood(model.forward(Xval)).covariance_matrix
-    if option_lv == "ind_ori":
+    if option_lv == "gp_ind_ori":
         SigmaH_list = []
         for task in range(0,n_task):
-            mk = model[task]
+            mk = model['task{}'.format(task+1)]
             SigmaH_k = mk.likelihood(mk.forward(Xval)).covariance_matrix
             SigmaH_list.append(SigmaH_k)
-        SigmaH = torch.block_diag(SigmaH_list)
+        SigmaH = torch.block_diag(*SigmaH_list)
 
-    
-    Kyi = torch.inverse(Wp@SigmaH@Wp.T+1e-6*torch.eye(n_task*Nval))
-    a = (yval.T@Kyi@yval)/(Nval*n_task)
+    Kyi = torch.inverse(Wp@SigmaH@Wp.T+1e-6*torch.eye(24*Nval))
+    a = (yval.T@Kyi@yval)/(Nval*24)
     
     return a
     

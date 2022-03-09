66208d36b57c7592002fb5779c1e57cf6c97ef63
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-09 11:24:02 -0700

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -10,11 +10,3 @@ def print_results(folder_data_name,RESULTS):
     
     
 
-    print('Mape Medio 24GPs indep   ', RESULTS[archivo]['mapemedio'])
-    print('R2 24GPs i:    ',RESULTS[archivo]['R224'])
-     
-    print('Confidence intervals : ', RESULTS[archivo]['ICs'])
-    print('Confidence intervals laplace 1 : ', RESULTS[archivo]['ICs_lap1'])
-    print('Confidence intervals laplace 2 : ', RESULTS[archivo]['ICs_lap2'])
-    print('Training time:   ',RESULTS[archivo]['training_time'] )
-    print('Test time:   ', RESULTS[archivo]['test_time'])

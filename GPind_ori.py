d43c81e4cfafe2dd0833f5c4d873f06f761b3b84
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-09 11:25:05 -0700

    Update GPind_ori.py
    Gneralize GPind_ori for multiple tasks.

--- GPind_ori.py
+++ GPind_ori.py
@@ -23,7 +23,7 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
 
       x= to_torch(x)
       y = to_torch(y)
-
+      n_tasks = y.size(1)
 
       learning_rate = opt_parameters['lr1']
       learning_rate2 = opt_parameters['lr2'] 
@@ -53,7 +53,7 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
           best_params = {}
           models = {}
           likelihoods = {}
-          for task in range(0,24):
+          for task in range(0,n_tasks):
               n_opt_iter = 0
               min_valid_loss = np.Inf
               min_train_loss = np.Inf
@@ -87,7 +87,7 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
               for it in range(0,num_iter):
                   optimizer.zero_grad()
                   train_loss,output = train_epoch(model,data_train_t,mll,optimizer)
-                  valid_loss,_ = valid_epoch(model,likelihood,output,data_val_t,mll)
+                  valid_loss,validation_error = valid_epoch(model,likelihood,output,data_val_t,mll)
               
                   train_loss = train_loss / data_train_t[0].size()[0]
                   valid_loss = valid_loss / data_val_t[0].size()[0]
@@ -103,6 +103,7 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
                   #if it> 1  and train_loss < np.min(history_t['train_loss']):
                   if it> 1  and valid_loss < np.min(history_t['valid_loss']):
                       min_valid_loss = valid_loss
+                      min_validation_error = validation_error
                       #min_train_loss = train_loss
                       n_opt_iter = it + 1
                       best_params_k = model.state_dict()
@@ -111,6 +112,7 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
                   history_t['valid_loss'].append(valid_loss)
               history_t ['n_opt_iter'] = n_opt_iter
               history_t ['min_valid_loss'] = min_valid_loss
+              history_t ['min_valid_error'] = min_validation_error
               history['task{}'.format(task+1)] = history_t  
               best_params['task{}'.format(task+1)] = best_params_k
               models['task{}'.format(task+1)] = model
@@ -123,17 +125,20 @@ def GPind_ori(x,y,n_tasks,kernel_type,opt_parameters):
           
           Opt_model = {}
           Opt_likelihood = {}
+          Validation_Errors = [None]*n_tasks
       print()
       print()
-      for task in range(0,24):  
+      for task in range(0,n_tasks):  
           Opt_loss = torch.inf
           for rest in range(0,n_restarts):
               minl_t_r = Results['restart{}'.format(rest+1)]['history']['task{}'.format(task+1)]['min_valid_loss']
+              min_val_error_t_r = Results['restart{}'.format(rest+1)]['history']['task{}'.format(task+1)]['min_valid_error']
               if minl_t_r<Opt_loss:
                   Opt_loss = minl_t_r
+                  min_val_error_t = min_val_error_t_r
                   Opt_model['task{}'.format(task+1)]= MODELS['restart{}'.format(rest+1)]['task{}'.format(task+1)]
                   Opt_likelihood['task{}'.format(task+1)]= LIKELIHOODS['restart{}'.format(rest+1)]['task{}'.format(task+1)]
               print('Task:{}'.format(task+1)+' Restart:{}'.format(rest)+' Min validation loss:{:.5f}'.format(Opt_loss))
-      
-              
+          Validation_Errors[task] = min_val_error_t 
+      Results['ValidationErrors']  =  Validation_Errors   
       return MODELS,LIKELIHOODS,Results,Opt_model,Opt_likelihood

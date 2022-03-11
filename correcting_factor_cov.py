483dee17c2297060fcc0f3ebff07d59d91af9650
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-10 20:44:09 -0700

    Update correcting_factor_cov.py
    Corrections:
    -Include the variance of the error estimation of NMF.
    - Error in the order of the tensor product of W (verify).
    -Unnormalize the covariance SigmaH.

--- correcting_factor_cov.py
+++ correcting_factor_cov.py
@@ -7,28 +7,32 @@ Created on Thu Feb 17 20:18:44 2022
 import numpy as np
 import torch
 from to_torch import to_torch
-from DeStandarizeData import DeStandarizeData
-def correcting_factor_cov(model,W,Yval,Xval,option_lv,scalerY_K):
+def correcting_factor_cov(model,Ws,Yval,Xval,option_lv,scalerY_K,Var_ErrorNMF,Std_nmf):
     # W: T x K
     # Yval: Nval x T
     # Xval  Nval x F
     # Kh : Nval x T  times  N x T
     
     Nval = Yval.size(0)
-    n_task = Yval.size(1)
+    n_task = len(model)
     yval = Yval.reshape(-1,1)
-    Wp = to_torch(np.kron(torch.eye(Nval),W))
+    W = Std_nmf*Ws
+    #Wp = to_torch(np.kron(torch.eye(Nval),W))
+    Wp = to_torch(np.kron(W,torch.eye(Nval)))
+    VarK = scalerY_K.var_
     if option_lv == "ind" or option_lv == "mt":
         SigmaH = model.likelihood(model.forward(Xval)).covariance_matrix
     if option_lv == "gp_ind_ori":
         SigmaH_list = []
         for task in range(0,n_task):
             mk = model['task{}'.format(task+1)]
-            SigmaH_k = mk.likelihood(mk.forward(Xval)).covariance_matrix
+            SigmaH_k = VarK[task]*(mk.likelihood(mk.forward(Xval)).covariance_matrix) # Varianzas solo en lugar de Cov? Rearrange de manera que se puedan operar diferente.
             SigmaH_list.append(SigmaH_k)
         SigmaH = torch.block_diag(*SigmaH_list)
 
-    Kyi = torch.inverse(Wp@SigmaH@Wp.T+1e-6*torch.eye(24*Nval))
+ # Las matrices deberian ser positivas definidas.
+    #Kyi = torch.inverse(Wp@SigmaH@Wp.T+1e-3*torch.eye(24*Nval)) # Noise estimation en lugar de eye
+    Kyi = torch.inverse(Wp@SigmaH@Wp.T+torch.diag(Var_ErrorNMF.repeat(Nval))) 
     a = (yval.T@Kyi@yval)/(Nval*24)
     
     return a

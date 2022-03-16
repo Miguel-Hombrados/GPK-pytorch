2b49f108ed567f30c9e65c24ad4726b3eff805be
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-15 20:56:04 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -71,29 +71,31 @@ from norm2laplace import norm2laplace
 from EvaluateConfidenceIntervals_Laplace import EvaluateConfidenceIntervals_Laplace
 from outliers_removal import outliers_removal
 from load_configuration import load_configuration
+from load_configuration_job_array import load_configuration_job_array
 from print_configuration import print_configuration
 from correcting_factor_cov import correcting_factor_cov
-from correcting_factor_cov_beta import correcting_factor_cov_beta
 from correcting_factor_cov_gamma import correcting_factor_cov_gamma
 from predictive_variance_white import predictive_variance_white
+from  print_extra_methods import  print_extra_methods
 from GP24I_v4 import GP24I
 from to_torch import to_torch
+from print_results_ic import print_results_ic
 # #Load Power Load Data =========================================================
 # #==============================================================================
 method = "NMF"  # Full
 methodfile = 'NMF'
 kernel_type = "rbf"
-forecast_method = "gp_ind_ori" # gp_ind_ori/gp_ind/gpk/gp_ind_laplace/gpmt
+forecast_method = "gpk" # gp_ind_ori/gp_ind/gpk/gp_ind_laplace/gpmt
 option_lv = "gp_ind_ori" # gp_ind_ori/gpmt
 if forecast_method == "gpk":
     name_forecast_method = forecast_method +"_" +option_lv
 else:
     name_forecast_method = forecast_method
-EXPERIMENT = 17#2  # This has to do with the verion of the NMF generated
+EXPERIMENT = 3  # This has to do with the verion of the NMF generated
 TaskNumber = 24
 Stand = True
-folder_data_name = "Exp_"+str(EXPERIMENT)
-#folder_data_name = "BuenosResNMF"
+#folder_data_name = "Exp_"+str(EXPERIMENT)
+folder_data_name = "BuenosResNMF"
 #LOCATIONS = ['ME','CT','NH','RI','NEMASSBOST','SEMASS','VT','WCMASS']
 
 datapath = Path.Path.joinpath(ProjectPath,"Data",folder_data_name)
@@ -101,7 +103,7 @@ DATAPATH = str(datapath)
 onlyfilesALL = [f for f in listdir(DATAPATH) if f.endswith('.pkl')]
 
 [onlyfiles,opt_parameters,forecast_method] = load_configuration(sys.argv,onlyfilesALL,forecast_method)    
-print_configuration(name_forecast_method,kernel_type,EXPERIMENT,Stand,folder_data_name)
+[onlyfiles,opt_parameters, forecast_method, option_lv] = load_configuration_job_array(sys.argv,onlyfilesALL)
 
 
 gpytorch.settings.max_cg_iterations._set_value(10000)
@@ -187,90 +189,40 @@ for archivo in range(len(onlyfiles)):
     end = time.time() 
     testing_time = end-start
     #=========================================================================
-    #[YPredicted_24gp_ind_S,VPredicted_24gp_ind_S] = predGPind(XTest_S,like_train_24ind,model_train_24ind)
-    #[YPredicted_24gp_ind_S,VPredicted_24gp_ind_S] = predGPind_lap(XTest_S,like_train_24ind,model_train_24ind)
-    if forecast_method == "gpk": 
 
+#==============================================================================
+#==============================================================================
+#==============================================================================
+    print_configuration(file_name,name_forecast_method,kernel_type,EXPERIMENT,Stand,folder_data_name)
+    if forecast_method == "gpk": 
         # TRANSFORMATION====
         S2norm = torch.pow(Stds_train_load,2)
         Snorm = Stds_train_load.T.repeat(Ntest,1)
         Snorm_tr  = Stds_train_load.T.repeat(Ntrain,1)
-     
-        ErrorValidation_std = torch.stack(RESULTS['ValidationErrors'],dim =1)
-        ErrorValidation_std_P = torch.stack(RESULTS['ValidationPredictiveErrors'],dim =1)
+        #ErrorValidation_std_P = torch.stack(RESULTS['ValidationPredictiveErrors'],dim =1)
         YPredicted_24gp = (YPredicted_24gp_K@WTrain.T)*Snorm
-      
-        Nval = ErrorValidation_std.size(0)
-        Snorm_val = Stds_train_load.T.repeat(Nval,1)
-
-        NoiseEstimation_Variance3  = torch.var((ErrorValidation_std@WTrain.T)*Snorm_val,axis=0) 
-        
         VPredicted_24gp = torch.zeros((Ntest,24))
-        
-        #ind_a = ind_val
-        ind_a = np.random.permutation(range(0,Ntrain))[0:100]
-        a = correcting_factor_cov(model,WTrain,YTrain_24[ind_a,:],XTrain_S[ind_a,:],option_lv,scalerY_K,NoiseEstimation_Variance3,Stds_train_load )
-        a_beta = correcting_factor_cov_beta(model,WTrain,YTrain_24[ind_a,:],XTrain_S[ind_a,:],option_lv,scalerY_K,NoiseEstimation_Variance3,Stds_train_load )
-        a_gamma = correcting_factor_cov_gamma(model,WTrain,YTrain_24[ind_a,:],XTrain_S[ind_a,:],option_lv,scalerY_K,NoiseEstimation_Variance3,Stds_train_load )
-        for ss in range(0,Ntest):
-            VPredicted_24gp[ss,:] = (torch.diag(WTrain@torch.diag(VPredicted_24gp_K[ss,:])@WTrain.T)*(S2norm.ravel())  +  NoiseEstimation_Variance3)*a
-        VPredicted_24gp_white = predictive_variance_white(VPredicted_24gp_K,WTrain,NoiseEstimation_Variance3,S2norm)
-    else:
+        # if 'ValidationErrors' in RESULTS:
+        #     ErrorValidation_std = torch.stack(RESULTS['ValidationErrors'],dim =1)
+        #     Nval = ErrorValidation_std.size(0)
+        #     Snorm_val = Stds_train_load.T.repeat(Nval,1)
+        #     NoiseEstimation_Variance3  = torch.var((ErrorValidation_std@WTrain.T)*Snorm_val,axis=0) 
+        #     ind_a = np.random.permutation(range(0,Ntrain))[0:100]
+        #     a = correcting_factor_cov(model,WTrain,YTrain_24[ind_a,:],XTrain_S[ind_a,:],option_lv,scalerY_K,NoiseEstimation_Variance3,Stds_train_load )
+        #     a_gamma = correcting_factor_cov_gamma(model,WTrain,YTrain_24[ind_a,:],XTrain_S[ind_a,:],option_lv,scalerY_K,NoiseEstimation_Variance3,Stds_train_load )
+        #     for ss in range(0,Ntest):
+        #         VPredicted_24gp[ss,:] = (torch.diag(WTrain@torch.diag(VPredicted_24gp_K[ss,:])@WTrain.T)*(S2norm.ravel())  +  NoiseEstimation_Variance3)*a
+        #     VPredicted_24gp_white = predictive_variance_white(VPredicted_24gp_K,WTrain,NoiseEstimation_Variance3,S2norm)
+        print_extra_methods(Stds_train_load,Ntest,Ntrain,WTrain,YTrain_24,YTest_24,XTrain_S,YPredicted_24gp_K,VPredicted_24gp_K,option_lv,scalerY_K,RESULTS,model,DATA)
+    elif forecast_method == "gp_ind_ori":
         [_, YPredicted_24gp,VPredicted_24gp]=DeStandarizeData(YTest_24_S,YPredicted_24gp_S,scalerY_24,VPredicted_24gp_S,Standarize = Stand)
-
-
-
-
-
-    ############################################################################
-    #[model_train_24ind,like_train_24ind,n_opt,min_valid_loss,_] = GPind(XTrain_S,YTrain_24_S,24,kernel_type)
-    #[model_train_24ind,like_train_24ind,history,best_params] = GPind_lap(XTrain_S,YTrain_24_S,24,kernel_type)
-    #[model_train_24ind,like_train_24ind,n_opt,min_valid_loss,ErrorValidation] = GPKtorch(XTrain_S,YTrain_24_S,24,kernel_type,option_lv)
-    
-    #K = ErrorValidation.size(2)
-    #Nfold = ErrorValidation.size(1)
-    #Nval = ErrorValidation.size(0)
- 
-    #EV = ErrorValidation.reshape(Nval*Nfold,K)
-    #Snorm_val = np.matlib.repmat(Stds_train_load.T,Nval,1)
-    #A = scipy.linalg.sqrtm(np.linalg.inv(WTrain.T@WTrain))
-    #NoiseEstimation_Variance3  = np.var((EV@A@WTrain.T)*Snorm_val,axis=0) 
-
-    #for ss in range(0,Ntest):
-    #    VPredicted_24gp_gpk[ss,:] = (np.diag(WTrain@A@np.diag(VPredicted_24gp_K[ss,:])@A@WTrain.T)*(S2norm.ravel())  +  NoiseEstimation_Variance3)#
-    # DENORMATLIZATION================================================================
-  
-    # 24GPKind================================================================
-    # start_gpk_ind = time.time()
-    #
-    # end_gpk_ind = time.time() 
-    # training_time_mtgp = end_gpk_ind-start_gpk_ind
-    
-    # start_gpk_ind = time.time()
-    # [YPredicted_Kgp_gpk_ind_S,VPredicted_Kgp_gpk_ind_S] = predGPind(XTest_S,like_train_24gpk_ind,model_train_24gpk_ind)
-    # [_, YPredicted_Kgp_gpk_ind,VPredicted_Kgp_gpk_ind]=DeStandarizeData(YTest_K_S,YPredicted_Kgp_gpk_ind_S,scalerY_K,VPredicted_Kgp_gpk_ind_S,Standarize = Stand)
-    # [YPredicted_24gp_gpk_ind,VPredicted_24gp_gpk_ind] = predGPK(YPredicted_Kgp_gpk_ind,VPredicted_Kgp_gpk_ind,WTrain,Stds_train_load = Stds_train_load)
-    # ## Estimation of the validation error in NMF
-    # YTrain_24_est = (WTrain@YTrain.T).T
-    # Etrain = YTrain_24_est - YTrain_24
-    # ##=========================================
-    # end_gpk_ind = time.time() 
-    # testing_time_mtgp = end_gpk_ind-start_gpk_ind
-    # ====================================================================  
-
-
+        [ICs,ICs_lap1,ICs_lap2] = print_results_ic(YPredicted_24gp,YTest_24,VPredicted_24gp,"gp_ind_ori")
     # METRICS==================================================================
-    [YPredicted_24gp_ind,Blap1] = norm2laplace(YPredicted_24gp,VPredicted_24gp,option=1)
-    [ICS1_24_l1,ICS2_24_l1] = EvaluateConfidenceIntervals_Laplace(YTest_24,YPredicted_24gp_ind,Blap1)    
-    [YPredicted_24gp,Blap2] = norm2laplace(YPredicted_24gp,VPredicted_24gp,option=2)
-    [ICS1_24_l2,ICS2_24_l2] = EvaluateConfidenceIntervals_Laplace(YTest_24,YPredicted_24gp,Blap2)    
-    [ICS1_24,ICS2_24] = EvaluateConfidenceIntervals(YTest_24,YPredicted_24gp,VPredicted_24gp)   
-
+    
     
     mapes= MAPE(YTest_24,YPredicted_24gp)
     mapemedio = torch.mean(mapes)
 
-     
     NTest = np.size(YTest_24,0)
     R2_all = np.zeros((NTest,1))
      
@@ -278,18 +230,21 @@ for archivo in range(len(onlyfiles)):
         R2_all[samp,0] = r2_score(YTest_24[samp,:],YPredicted_24gp[samp,:])
     r2_24gp  = np.mean(R2_all)
     
-    
+ 
     # PRINT===================================================================
+
+    
+    
     print('Mape Medio 24GPs indep   ', mapemedio )
     print('R2 24GPs i:    ',r2_24gp)
     
-    print('Confidence intervals Mean  : ',(torch.mean(ICS1_24.T),torch.mean(ICS2_24.T)))
-    print('Confidence intervals laplace 1  Mean: ', (torch.mean(ICS1_24_l1.T),torch.mean(ICS2_24_l1.T)))
-    print('Confidence intervals laplace 2 Mean : ', (torch.mean(ICS1_24_l2.T),torch.mean(ICS2_24_l2.T)))
-     
-    print('Confidence intervals : ',(ICS1_24.T,ICS2_24.T))
-    print('Confidence intervals laplace 1 : ', (ICS1_24_l1.T,ICS2_24_l1.T))
-    print('Confidence intervals laplace 2 : ', (ICS1_24_l2.T,ICS2_24_l2.T))
+    
+    if 'ValidationErrors' in RESULTS:
+        Lval = RESULTS['ValidationErrors']
+        print_extra_methods(Stds_train_load,Ntest,Ntrain,WTrain,YTrain_24,YTest_24,XTrain_S,YPredicted_24gp_K,VPredicted_24gp_K,option_lv,scalerY_K,RESULTS,model,DATA)
+        Lval_tasks = [torch.mean(x) for x in Lval]
+        Lval_mean = torch.mean(torch.tensor(Lval_tasks))
+        print('Mean validation loss ',Lval_mean)
     
     
     print('Training time:   ', training_time )
@@ -303,9 +258,9 @@ for archivo in range(len(onlyfiles)):
     Results['Ypred'] = YPredicted_24gp
     Results['Vpred'] = VPredicted_24gp
     Results['likelihood'] = like
-    Results['ICs'] = (ICS1_24,ICS2_24)
-    Results['ICs_lap1'] = (ICS1_24_l1,ICS2_24_l1)
-    Results['ICs_lap2'] = (ICS1_24_l2,ICS2_24_l2)
+    #Results['ICs'] = ICs
+    #Results['ICs_lap1'] = ICs_lap1
+    #Results['ICs_lap2'] = ICs_lap2
     
     if forecast_method == "gpk":
         Results['Wtrain'] = WTrain
@@ -318,7 +273,7 @@ for archivo in range(len(onlyfiles)):
     file_model = Path.Path.joinpath(ResultsPath,file_name+"_model")
     file_data = Path.Path.joinpath(ResultsPath,file_name+"_data")
     #save_obj(RESULTS, file_results.as_posix())
-    #save_obj(RESULTS, file_model.as_posix())
+    #save_obj(model, file_model.as_posix())
     #save_obj(DATA, file_data.as_posix())
 
 

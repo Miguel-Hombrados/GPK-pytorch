ad8b6258700fe6a3324f6d9da87bdfadef93fe3a
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-15 20:54:01 -0600

    Update print_configuration.py
    Update function that prints the configuration parameters.

--- print_configuration.py
+++ print_configuration.py
@@ -4,11 +4,12 @@ Created on Mon Mar  7 10:56:05 2022
 
 @author: mahom
 """
-def print_configuration(forecast_method,kernel_type,EXPERIMENT,Stand,folder_data_name):
-    forecast_method
-    print("Forecasting method:", forecast_method)
+def print_configuration(file_name,name_forecast_method,kernel_type,EXPERIMENT,Stand,folder_data_name):
+    print("Filename/Location:", file_name)
+    print("Forecasting method:", name_forecast_method)
     print("Kernel type:", kernel_type)
     print("Is data Standarized:", Stand)
-    print("Data folder:", folder_data_name)
     print("Experiment:",   EXPERIMENT)
+    print('NMF data: ', folder_data_name)
+    print('location')
     

a19f030c3736aaf64d04fdf7c59fdd8490a25db7
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-15 21:49:32 -0600

    Update load_configuration_job_array.py
    Option to load parameters from jobarray

--- load_configuration_job_array.py
+++ load_configuration_job_array.py
@@ -11,21 +11,25 @@ Created on Tue Mar 15 13:45:03 2022
 import sys
 import pandas as pd
 import pathlib as Path
+from itertools import product
 def load_configuration_job_array(input_params,onlyfilesALL):
   # index of the job array
     opt_parameters ={}
     ProjectPath = Path.Path.cwd()
     file_path = Path.Path.joinpath(ProjectPath,'parameters.xlsx')
     df = pd.read_excel(file_path)
+    uniques = [df[i].unique().tolist() for i in df.columns ]
+    dfcombinated = pd.DataFrame(product(*uniques), columns = df.columns)
+    dfcombinated.dropna(inplace=True)
     index = sys.argv[1]
-    forecast_method = df[index,'methods']
-    method_lv = df[index,'method_lv']
-    opt_parameters['lr1'] = float(df[index,'lr'])
-    opt_parameters['lr2'] = float(df[index,'lr2'])
-    opt_parameters['n_restarts'] = int(df[index,'nrestarts'])
-    opt_parameters['num_iter'] = int(df[index,'niter'])
-    opt_parameters['trainsize'] = float(df[index,'trainsize'])
-    location = "_"+str(df[index,'location'])+"_"
+    forecast_method = dfcombinated[index,'methods']
+    method_lv = dfcombinated[index,'method_lv']
+    opt_parameters['lr1'] = float(dfcombinated[index,'lr'])
+    opt_parameters['lr2'] = float(dfcombinated[index,'lr2'])
+    opt_parameters['n_restarts'] = int(dfcombinated[index,'nrestarts'])
+    opt_parameters['num_iter'] = int(dfcombinated[index,'niter'])
+    opt_parameters['trainsize'] = float(dfcombinated[index,'trainsize'])
+    location = "_"+str(dfcombinated[index,'location'])+"_"
     onlyfiles = [f for f in onlyfilesALL if location in f ]
     
     print("Forecasting method:", forecast_method)

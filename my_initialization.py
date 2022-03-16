cf6b182657120ad54def93b090a8dd5c98609093
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-15 20:54:37 -0600

    Update my_initialization.py

--- my_initialization.py
+++ my_initialization.py
@@ -25,20 +25,15 @@ def my_initialization(model,likelihood,kernel_type,task_num,method):
     elif method == "gpi_ori": 
         if kernel_type == "linear":
              #model.likelihood.noise = torch.tensor(0.1)
-             model.likelihood.noise = 1e-1*torch.ones(task_num,requires_grad=True)                               
+             model.likelihood.noise = 0.5*torch.ones(task_num,requires_grad=True)                               
              model.covar_module.kernels[0].base_kernel.outputscale = 10*torch.ones(task_num,requires_grad=True)             
              model.covar_module.kernels[0].base_kernel.variance = 10*torch.ones(task_num,1,1,requires_grad=True)             
              model.covar_module.kernels[1].bias = 1e-5*torch.ones(task_num,1,1,requires_grad=True)         
         if kernel_type == "rbf":
-            model.covar_module.kernels[0].outputscale = 45*torch.ones(task_num,requires_grad=True)  
-            model.likelihood.noise = 0.1*torch.ones(task_num,requires_grad=True)                                                        # 0.5
-            model.covar_module.kernels[0].base_kernel.lengthscale = 35*torch.ones(task_num,1,1,requires_grad=True) # probar 96.5                #3
+            model.covar_module.kernels[0].outputscale = 25*torch.ones(task_num,requires_grad=True)  
+            model.likelihood.noise = 1*torch.ones(task_num,requires_grad=True)                                                        # 0.5
+            model.covar_module.kernels[0].base_kernel.lengthscale = 50*torch.ones(task_num,1,1,requires_grad=True) # probar 96.5                #3
             model.covar_module.kernels[1].bias = 0.5*torch.ones(task_num,requires_grad=True)  
             #model.mean_module.constant = torch.nn.Parameter(torch.tensor(0.))
             model.mean_module.initialize(constant=0.)
-            #model.mean_module.constant = torch.zeros(())
-            
-            #model.covar_module.outputscale = 45*torch.ones(task_num,requires_grad=True)  
-            #model.likelihood.task_noises = 0.1*torch.ones(task_num,requires_grad=True)                                                        # 0.5
-            #model.covar_module.base_kernel.lengthscale = 35*torch.ones(task_num,1,1,requires_grad=True) # probar 96.5 
-
+      

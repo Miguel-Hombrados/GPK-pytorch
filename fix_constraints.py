9c1c64751d42366e3d97c6cfcc573cf847b20651
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-17 21:36:31 -0600

    Update fix_constraints.py

--- fix_constraints.py
+++ fix_constraints.py
@@ -28,15 +28,12 @@ def fix_constraints(model,likelihood,kernel_type,num_task,method):
             model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-8, 1))                       #(1e-6,1)
     elif method == "gpi_ori":
         if kernel_type == "rbf":  
-            model.likelihood.noise_covar.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-4, 10))                                                            # 0.5    
+            model.likelihood.noise_covar.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-4, 1))                                                            # 0.5    
             model.covar_module.kernels[0].register_constraint("raw_outputscale", gpytorch.constraints.Interval(1e-5, 200))
-            model.covar_module.kernels[0].base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(1,200))                  #3
+            model.covar_module.kernels[0].base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(1,100))                  #3
             model.covar_module.kernels[1].register_constraint("raw_bias", gpytorch.constraints.Interval(1e-7, 1))   
                 
-                #model.likelihood.noise_covar.register_constraint("raw_noise", gpytorch.constraints.Interval(1e-4, 10))                                                            # 0.5    
-                #model.covar_module.register_constraint("raw_outputscale", gpytorch.constraints.Interval(1e-5, 50))
-                #model.covar_module.base_kernel.register_constraint("raw_lengthscale", gpytorch.constraints.Interval(1,100))    
-                
+
 
                
                 

f45c8258b8c54912fa42c12ef6233ca8737130f5
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-19 19:07:51 -0600

    Create del_test_correcting_factor.py

---
+++ del_test_correcting_factor.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 21:50:33 2022
+
+@author: mahom
+"""
+
+from print_extra_methods import print_extra_methods
+print_extra_methods(Stds_train_load,Ntest,Ntrain,WTrain,YTrain_24,YTest_24,XTrain_S,YPredicted_24gp_K,VPredicted_24gp_K,option_lv,scalerY_K,RESULTS,model,DATA)

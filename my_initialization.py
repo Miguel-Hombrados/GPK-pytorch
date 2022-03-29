a2075f700e1db83440ca041b517722d321d3b118
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-29 14:09:12 -0600

    Update my_initialization.py
    Add option for GPMT and GPK sparse.

--- my_initialization.py
+++ my_initialization.py
@@ -36,4 +36,20 @@ def my_initialization(model,likelihood,kernel_type,task_num,method):
             model.covar_module.kernels[1].bias = 0.5*torch.ones(task_num,requires_grad=True)  
             #model.mean_module.constant = torch.nn.Parameter(torch.tensor(0.))
             model.mean_module.initialize(constant=0.)
-      
+    elif method == "gpk_sp":
+        if kernel_type == "rbf":
+            model.covar_module.kernels[0].outputscale = 25*torch.ones(task_num,requires_grad=True)  
+            model.likelihood.noise = 1*torch.ones(task_num,requires_grad=True)                                                        # 0.5
+            model.covar_module.kernels[0].base_kernel.lengthscale = 50*torch.ones(task_num,1,1,requires_grad=True) # probar 96.5                #3
+            model.covar_module.kernels[1].bias = 0.5*torch.ones(task_num,requires_grad=True)  
+            #model.mean_module.constant = torch.nn.Parameter(torch.tensor(0.))
+            model.mean_module.initialize(constant=0.)
+    elif method == "gpmt":
+        if kernel_type == "rbf":
+            model.likelihood.noise = torch.tensor([1e-6]).requires_grad_(True)                                            # 0.5
+            model.covar_module.covar_module_list[0].data_covar_module.kernels[0].outputscale = 25*torch.ones(task_num,requires_grad=True)  
+            model.likelihood.task_noises = 1*torch.ones(task_num,requires_grad=True)                                                        # 0.5
+            model.covar_module.covar_module_list[0].data_covar_module.kernels[0].base_kernel.lengthscale = 50*torch.ones(task_num,1,1,requires_grad=True) # probar 96.5                #3
+            model.covar_module.covar_module_list[0].data_covar_module.kernels[1].bias = 0.5*torch.ones(task_num,requires_grad=True)  
+            for t in range(0,task_num):
+                model.mean_module.base_means[t].constant = torch.nn.Parameter(torch.tensor(0.))
